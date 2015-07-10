f5e0bda5e554d48658d3633baefaedb9636532fe
Patrick Trasborg <patrick.trasborg@gmail.com>
2015-07-10 09:44:01 -0400

    Server now outputting urls in addition to sport name

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var request = require('request').defaults({followAllRedirects: true, jar: true});
 var cheerio = require('cheerio');
+var send = require('./email.js');
 
 url = "https://www.cosport.com/olympics/tickets.aspx";
 
@@ -17,23 +18,28 @@ setInterval(function(){
 	});
 }, 10000);
 
-var ticketOffering;
+var ticketOffering = {};
 function ticketSave(html){
 	$ = cheerio.load(html);
 	var change = false;
 	ticketOffering = [];
 	$('tbody').children('tr').children('td').children('b').children('a').each(function(i, elem){
-		var numberEvents = $(this).parentsUntil('tr').children('td').children('a').length;
+		var eventNames = $(this).parentsUntil('tr').children('td').children('a');
+		var numberEvents = eventNames.length;
 		if(events[$(this).text()] !== numberEvents || !events[$(this).text()]){
 			events[$(this).text()] = numberEvents;
 			change = true;
-			ticketOffering.push($(this).text());
-		}else{
-			change = false;
+			var name = $(this).text();
+			ticketOffering[name] = [];
+			eventNames.each(function(i, elem){
+				ticketOffering[name].push("https://www.cosport.com" + $(this).attr('href'));
+			});
 		}
 	});
-	if(change) console.log(events);
-	else console.log("No change this time");
+	if(change){
+		console.log(ticketOffering);
+		// send(ticketOffering);
+	}else console.log("No change this time");
 }
 
-module.exports = ticketOffering;
+// module.exports = ticketOffering;
